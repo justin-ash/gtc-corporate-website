e2c6073a646568f30d02f3f984da7177608c5065
= <=>
2026-08-25 23:21:22 +0530

    url change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -77,7 +77,7 @@ class HomeController extends Controller
     public function services()
     {
         $widgets = Widget::formatWidgets(['companies', 'header', 'footer', 'sidebar']);
-        $seo = SeoPage::seoByPage('companies');
+        $seo = SeoPage::seoByPage('our-expertise');
         $services = Service::where('is_active', 1)->get();
 
         return view('companies', compact('widgets', 'seo', 'services'));
